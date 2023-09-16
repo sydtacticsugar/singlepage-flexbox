799a39ea448cac559c06f5fd75ae828b80eeaaf3
syd <45886944+sydtacticsugar@users.noreply.github.com>
2023-09-15 23:32:42 -0600

    stopped default nav behavior and replaced it with a scrolling animation in js

--- zone.js
+++ zone.js
@@ -5,11 +5,38 @@ navLinks.forEach((el)=>{
     console.log(el);
     el.addEventListener('click',(e)=>{
         console.log(e);
-        //e.preventDefault();
+        e.preventDefault();
+        // update active nav item
         let remover = document.querySelector('.active');
         if(remover != null){
             remover.classList.remove('active');
         }
         el.classList.add('active');
+        console.log(el.hash);
+        let stopPoint = document.querySelector(el.hash).offsetTop;
+        let startPoint = document.documentElement.scrollTop;
+        console.log(stopPoint);
+        console.log(startPoint);
+        moveMe(startPoint,stopPoint);
     })
 })
+
+function moveMe(startPoint,stopPoint){
+    const speed = 10;
+    const pos = stopPoint > startPoint ? 1 : -1;
+    let move = Math.floor((stopPoint - startPoint)*pos/speed);
+    const addIt = pos * speed;
+    console.log(move,addIt);
+    let framed = setInterval(frame,1);
+    function frame(){
+        if(move<0){
+            clearInterval(framed);
+        }
+        else {
+            move--;
+            startPoint = startPoint + addIt;
+            document.documentElement.scrollTop = startPoint;
+            console.log(startPoint);
+        }
+    }
+}
